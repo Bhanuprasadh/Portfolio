9c313042e4cd459b185bd708872833818d099761
Bhanuprasadh Santra <91531986+Bhanuprasadh@users.noreply.github.com>
2024-09-26 12:34:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
-const typed = new typed('.multiple-text',  {
+const typed = new Typed('.multiple-text',  {
     strings: ['Machine Learning Engineer', 'Data Scientist', 'MLOps Engineer'],
     typeSpeed: 80,
     backSpeed: 80,
