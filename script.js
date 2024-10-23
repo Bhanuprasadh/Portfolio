c55ca4861d89e919b6072482c28916ae43439b56
Bhanuprasadh <bhanuprasadh004@gmail.com>
2024-10-23 18:10:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.onscroll = () => {
 }
 
 const typed = new Typed('.multiple-text',  {
-    strings: ['Machine Learning Engineer', 'Data Scientist', 'MLOps Engineer'],
+    strings: ['Machine Learning Engineer', 'Data Scientist', 'MLOps Engineer', 'Data Analyst'],
     typeSpeed: 80,
     backSpeed: 80,
     backDelay: 1200,
